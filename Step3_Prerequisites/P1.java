e4a5415411217e6a2036f935029f4fc39b87f34e
Azaan Suhail <azaan.suhail@gmail.com>
2025-10-08 23:59:23 +0530

    Step 3 DSA_Prerequisites ✅

---
+++ Step3_Prerequisites/P1.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+import java.util.*;
+public class P1 {
+
+    public static  void  Sieve_Of_Eratosthenes(int n){
+        //Step1
+        int arr[]=new int[n+1];
+
+
+        //Step2 : Considering all are primes
+        for(int i=2;i<n+1;++i){
+            arr[i]=1;
+        }
+
+        //Step3
+        for(int i=2;i<=(int)Math.sqrt(n+1);++i){
+            if(arr[i]==1){ 
+                //Unmarked all its multiples as non-primes
+                for(int j=i*i;j<=n;j=j+i){
+                    arr[j]=0;
+                }
+
+            }
+        }
+
+        int count=0;
+        for(int i=2;i<=n;++i){
+            if(arr[i]==1){
+                count++;
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println("\nCount="+count);
+    }
+    public static void main(String[] args) {
+        Sieve_Of_Eratosthenes(13);
+
+    }
+}
